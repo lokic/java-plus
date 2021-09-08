7423c2fe8e2a0e2a8020d5ec362592793f2ef983
loki <kiraconan2046@gmail.com>
2021-09-08 22:48:05 +0800

    test: add CompletableFutures For method exception

--- src/test/java/com/github/lokic/javaplus/CompletableFuturesTest.java
+++ src/test/java/com/github/lokic/javaplus/CompletableFuturesTest.java
@@ -11,13 +11,28 @@ import static com.github.lokic.javaplus.CompletableFutures.Fors.Yield;
 public class CompletableFuturesTest {
 
     @Test
-    public void test_For() {
+    public void test_For_success() {
         CompletableFuture<String> r = CompletableFuture.completedFuture("1")
                 .thenCompose(For(t1 -> CompletableFuture.completedFuture("2")))
                 .thenCompose(For((t1, t2) -> CompletableFuture.completedFuture("3")))
                 .thenCompose(For((t1, t2, t3) -> CompletableFuture.completedFuture("4")))
                 .thenApply(Yield((t1, t2, t3, t4) -> t1 + t2 + t3 + t4));
         Assert.assertEquals("1234", r.join());
+    }
+
+    @Test
+    public void test_For_exception() {
+        CompletableFuture<String> r = CompletableFuture.completedFuture("1")
+                .thenCompose(For(t1 -> CompletableFuture.completedFuture("2")))
+                .thenCompose(For((t1, t2) -> {
+                    CompletableFuture<String> f = new CompletableFuture<>();
+                    f.completeExceptionally(new RuntimeException("test"));
+                    return f;
+                }))
+                .thenCompose(For((t1, t2, t3) -> CompletableFuture.completedFuture("4")))
+                .thenApply(Yield((t1, t2, t3, t4) -> t1 + t2 + t3 + t4));
+
+        Assert.assertTrue(r.isCompletedExceptionally());
     }
 
 }
