a1b0a0b8930a01e7885facc2970e53787eb1f175
loki <kiraconan2046@gmail.com>
2021-09-18 17:19:37 +0800

    feat(CompletableFutures): add new methods

--- src/main/java/com/github/lokic/javaplus/CompletableFutures.java
+++ src/main/java/com/github/lokic/javaplus/CompletableFutures.java
@@ -6,22 +6,72 @@ import com.github.lokic.javaplus.func.function.Function4;
 import com.github.lokic.javaplus.func.function.Function5;
 import com.github.lokic.javaplus.func.tuple.*;
 import com.github.lokic.javaplus.tuple.*;
+import lombok.SneakyThrows;
 
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class CompletableFutures {
 
     public static <T> CompletableFuture<List<T>> sequence(List<CompletableFuture<T>> futures) {
-        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]))
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
                 .thenApply(v -> futures.stream().map(CompletableFuture::join).collect(java.util.stream.Collectors.<T>toList()));
     }
 
     public static <K, V> CompletableFuture<Map<K, V>> sequence(Map<K, CompletableFuture<V>> futures) {
-        return CompletableFuture.allOf(futures.values().toArray(new CompletableFuture[futures.size()]))
+        return CompletableFuture.allOf(futures.values().toArray(new CompletableFuture[0]))
                 .thenApply(v -> futures.entrySet().stream().collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, e -> e.getValue().join())));
+    }
+
+    public static <T> CompletableFuture<T> supply(Supplier<T> supplier) {
+        CompletableFuture<T> future = new CompletableFuture<>();
+        try {
+            T data = supplier.get();
+            future.complete(data);
+        } catch (Throwable ex) {
+            future.completeExceptionally(ex);
+        }
+        return future;
+    }
+
+    public static <T> CompletableFuture<T> call(Callable<T> callable) {
+        CompletableFuture<T> future = new CompletableFuture<>();
+        try {
+            T data = callable.call();
+            future.complete(data);
+        } catch (Throwable ex) {
+            future.completeExceptionally(ex);
+        }
+        return future;
+    }
+
+
+    @SneakyThrows
+    public static <T> T getOrElseSneakyThrow(CompletableFuture<T> future) {
+        try {
+            return future.get();
+        } catch (ExecutionException e) {
+            throw e.getCause();
+        }
+    }
+
+    public static <T> T getOrElseThrow(CompletableFuture<T> future) throws Throwable {
+        return getOrElseThrow(future, Function.identity());
+    }
+
+    public static <T, X extends Throwable> T getOrElseThrow(CompletableFuture<T> future, Function<? super Throwable, X> exceptionProvider) throws X {
+        try {
+            return future.get();
+        } catch (ExecutionException e) {
+            throw exceptionProvider.apply(e.getCause());
+        } catch (Throwable e) {
+            throw exceptionProvider.apply(e);
+        }
     }
 
     public static class Fors {
@@ -33,7 +83,6 @@ public class CompletableFutures {
         public static <T1, T2> Function<T1, CompletableFuture<Tuple2<T1, T2>>> For(Function<T1, CompletableFuture<T2>> f) {
             return t1 -> f.apply(t1).thenApply(t2 -> Tuple.of(t1, t2));
         }
-
 
         public static <T1, T2, T3> Function<Tuple2<T1, T2>, CompletableFuture<Tuple3<T1, T2, T3>>> For(Function2<T1, T2, CompletableFuture<T3>> f) {
             return t -> f.apply(t.getT1(), t.getT2()).thenApply(t3 -> Tuple.of(t.getT1(), t.getT2(), t3));
@@ -50,7 +99,6 @@ public class CompletableFutures {
         public static <T1, T2, T3, T4, T5, T6> Function<Tuple5<T1, T2, T3, T4, T5>, CompletableFuture<Tuple6<T1, T2, T3, T4, T5, T6>>> For(Function5<T1, T2, T3, T4, T5, CompletableFuture<T6>> f) {
             return t -> f.apply(t.getT1(), t.getT2(), t.getT3(), t.getT4(), t.getT5()).thenApply(t6 -> Tuple.of(t.getT1(), t.getT2(), t.getT3(), t.getT4(), t.getT5(), t6));
         }
-
 
         public static <T1, R> Function<T1, R> Yield(Function<T1, R> f) {
             return f;
--- src/test/java/com/github/lokic/javaplus/CompletableFuturesTest.java
+++ src/test/java/com/github/lokic/javaplus/CompletableFuturesTest.java
@@ -1,14 +1,41 @@
 package com.github.lokic.javaplus;
 
+import org.assertj.core.util.Lists;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import static com.github.lokic.javaplus.CompletableFutures.Fors.For;
 import static com.github.lokic.javaplus.CompletableFutures.Fors.Yield;
 
 public class CompletableFuturesTest {
+
+    @Test
+    public void test_list_sequence() {
+        List<CompletableFuture<String>> li = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            li.add(CompletableFuture.completedFuture(String.valueOf(i)));
+        }
+        Assert.assertEquals(Lists.newArrayList("0", "1", "2", "3", "4"), CompletableFutures.sequence(li).join());
+    }
+
+
+    @Test
+    public void test_map_sequence() {
+        Map<Integer, CompletableFuture<String>> map = new HashMap<>();
+        Map<Integer, String> expect = new HashMap<>();
+        for (int i = 0; i < 5; i++) {
+            map.put(i, CompletableFuture.completedFuture(String.valueOf(i)));
+            expect.put(i, String.valueOf(i));
+        }
+        Assert.assertEquals(expect, CompletableFutures.sequence(map).join());
+    }
 
     @Test
     public void test_For_success() {
@@ -33,6 +60,43 @@ public class CompletableFuturesTest {
                 .thenApply(Yield((t1, t2, t3, t4) -> t1 + t2 + t3 + t4));
 
         Assert.assertTrue(r.isCompletedExceptionally());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_getOrElseSneakyThrow_exception() {
+        CompletableFuture<String> f = new CompletableFuture<>();
+        f.completeExceptionally(new IllegalStateException());
+        CompletableFutures.getOrElseSneakyThrow(f);
+    }
+
+    @Test(expected = IOException.class)
+    public void test_getOrElseThrow_exception() throws Throwable {
+        CompletableFuture<String> f = new CompletableFuture<>();
+        f.completeExceptionally(new IOException());
+        CompletableFutures.getOrElseThrow(f);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_getOrElseThrow_exceptionProvider_exception() {
+        CompletableFuture<String> f = new CompletableFuture<>();
+        f.completeExceptionally(new IOException());
+        CompletableFutures.getOrElseThrow(f, IllegalStateException::new);
+    }
+
+    @Test
+    public void test_supply() {
+        Assert.assertEquals("123", CompletableFutures.supply(() -> "123").join());
+        Assert.assertTrue(CompletableFutures.supply(() -> {
+            throw new IllegalStateException();
+        }).isCompletedExceptionally());
+    }
+
+    @Test
+    public void test_call() {
+        Assert.assertEquals("123", CompletableFutures.call(() -> "123").join());
+        Assert.assertTrue(CompletableFutures.call(() -> {
+            throw new IOException();
+        }).isCompletedExceptionally());
     }
 
 }
