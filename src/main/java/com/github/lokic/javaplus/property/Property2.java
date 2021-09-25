63a736db86a15944d15fe92a2f7eae7efd9e0ed4
loki <kiraconan2046@gmail.com>
2021-09-25 13:18:36 +0800

    feat: add Property2 and Property3

---
+++ src/main/java/com/github/lokic/javaplus/property/Property2.java
@@ -0,0 +1,35 @@
+package com.github.lokic.javaplus.property;
+
+import com.github.lokic.javaplus.tuple.Tuple;
+import com.github.lokic.javaplus.tuple.Tuple2;
+import lombok.NonNull;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * 用于K1和K2二个值作为key来查询枚举的场景
+ *
+ * @param <E>
+ * @param <K1>
+ * @param <K2>
+ */
+public class Property2<E extends Enum<E>, K1, K2> {
+    private final Property1<E, Tuple2<K1, K2>> property;
+
+    public Property2(@NonNull Class<E> clazz, @NonNull Function<E, Tuple2<K1, K2>> function) {
+        this.property = new Property1<>(clazz, function);
+    }
+
+    public E of(K1 k1, K2 k2) {
+        return property.of(Tuple.of(k1, k2));
+    }
+
+    public Optional<E> optOf(K1 k1, K2 k2) {
+        return property.optOf(Tuple.of(k1, k2));
+    }
+
+    public E requireOf(K1 k1, K2 k2) {
+        return property.requireOf(Tuple.of(k1, k2));
+    }
+}
---
+++ src/main/java/com/github/lokic/javaplus/property/Property3.java
@@ -0,0 +1,36 @@
+package com.github.lokic.javaplus.property;
+
+import com.github.lokic.javaplus.tuple.Tuple;
+import com.github.lokic.javaplus.tuple.Tuple3;
+import lombok.NonNull;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * 用于K1、K2、K3三个值作为key来查询枚举的场景
+ *
+ * @param <E>
+ * @param <K1>
+ * @param <K2>
+ * @param <K3>
+ */
+public class Property3<E extends Enum<E>, K1, K2, K3> {
+  private final Property1<E, Tuple3<K1, K2, K3>> property;
+
+  public Property3(@NonNull Class<E> clazz, @NonNull Function<E, Tuple3<K1, K2, K3>> function) {
+    this.property = new Property1<>(clazz, function);
+  }
+
+  public E of(K1 k1, K2 k2, K3 k3) {
+    return property.of(Tuple.of(k1, k2, k3));
+  }
+
+  public Optional<E> optOf(K1 k1, K2 k2, K3 k3) {
+    return property.optOf(Tuple.of(k1, k2, k3));
+  }
+
+  public E requireOf(K1 k1, K2 k2, K3 k3) {
+    return property.requireOf(Tuple.of(k1, k2, k3));
+  }
+}
