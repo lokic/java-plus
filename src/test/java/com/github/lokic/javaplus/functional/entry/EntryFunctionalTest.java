0867dbd9d51e884cb756878f9fbb9a4868c50b5b
loki <kiraconan2046@gmail.com>
2021-11-22 21:17:14 +0800

    feat: 支持EntryFunctional#function(Function3)

--- src/test/java/com/github/lokic/javaplus/functional/entry/EntryFunctionalTest.java
+++ src/test/java/com/github/lokic/javaplus/functional/entry/EntryFunctionalTest.java
@@ -1,8 +1,13 @@
 package com.github.lokic.javaplus.functional.entry;
 
+import com.github.lokic.javaplus.tuple.Tuple;
+import com.github.lokic.javaplus.tuple.Tuple2;
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,7 +20,7 @@ public class EntryFunctionalTest {
         ageMaps.put(Stream.of("A"), Stream.of(19));
         ageMaps.put(Stream.of("B"), Stream.of(20));
         ageMaps.put(Stream.of("C"), Stream.of(20));
-        
+
         // EntryFunction
         ageMaps.entrySet().stream()
                 .map(EntryFunctional.function(this::zipEntryTest))
@@ -24,6 +29,20 @@ public class EntryFunctionalTest {
         // EntryConsumer
         ageMaps.entrySet().stream()
                 .forEach(EntryFunctional.consumer(this::zipEntryTest));
+    }
+
+    @Test
+    public void test_function3() {
+        Map<Tuple2<Integer, String>, Long> map = new LinkedHashMap<>();
+        map.put(Tuple.of(1, "A"), 11L);
+        map.put(Tuple.of(2, "B"), 22L);
+        map.put(Tuple.of(3, "C"), 33L);
+
+        List<String> re = map.entrySet().stream()
+                .map(EntryFunctional.function((a, b, c) -> a + " " + b + " " + c))
+                .collect(Collectors.toList());
+        Assertions.assertThat(re)
+                .containsExactly("1 A 11", "2 B 22", "3 C 33");
     }
 
     public <A, B> Stream<Map.Entry<A, B>> zipEntryTest(Stream<A> streamA, Stream<B> streamB) {
