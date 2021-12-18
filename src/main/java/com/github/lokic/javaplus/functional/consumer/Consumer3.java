4ad53848728b84db91ea818820e136b208b90058
loki <kiraconan2046@gmail.com>
2021-12-19 00:27:28 +0800

    feat: Consumer支持特定索引参数的函数，避免编写不关注的变量名

--- src/main/java/com/github/lokic/javaplus/functional/consumer/Consumer3.java
+++ src/main/java/com/github/lokic/javaplus/functional/consumer/Consumer3.java
@@ -1,6 +1,21 @@
 package com.github.lokic.javaplus.functional.consumer;
 
+import java.util.function.Consumer;
+
 @FunctionalInterface
 public interface Consumer3<T1, T2, T3> {
     void accept(T1 t1, T2 t2, T3 t3);
+
+
+    static <T1, T2, T3> Consumer3<T1, T2, T3> $1(Consumer<T1> consumer) {
+        return (t1, t2, t3) -> consumer.accept(t1);
+    }
+
+    static <T1, T2, T3> Consumer3<T1, T2, T3> $2(Consumer<T2> consumer) {
+        return (t1, t2, t3) -> consumer.accept(t2);
+    }
+
+    static <T1, T2, T3> Consumer3<T1, T2, T3> $3(Consumer<T3> consumer) {
+        return (t1, t2, t3) -> consumer.accept(t3);
+    }
 }
--- src/main/java/com/github/lokic/javaplus/functional/consumer/Consumer4.java
+++ src/main/java/com/github/lokic/javaplus/functional/consumer/Consumer4.java
@@ -1,6 +1,24 @@
 package com.github.lokic.javaplus.functional.consumer;
 
+import java.util.function.Consumer;
+
 @FunctionalInterface
 public interface Consumer4<T1, T2, T3, T4> {
     void accept(T1 t1, T2 t2, T3 t3, T4 t4);
+
+    static <T1, T2, T3, T4> Consumer4<T1, T2, T3, T4> $1(Consumer<T1> consumer) {
+        return (t1, t2, t3, t4) -> consumer.accept(t1);
+    }
+
+    static <T1, T2, T3, T4> Consumer4<T1, T2, T3, T4> $2(Consumer<T2> consumer) {
+        return (t1, t2, t3, t4) -> consumer.accept(t2);
+    }
+
+    static <T1, T2, T3, T4> Consumer4<T1, T2, T3, T4> $3(Consumer<T3> consumer) {
+        return (t1, t2, t3, t4) -> consumer.accept(t3);
+    }
+
+    static <T1, T2, T3, T4> Consumer4<T1, T2, T3, T4> $4(Consumer<T4> consumer) {
+        return (t1, t2, t3, t4) -> consumer.accept(t4);
+    }
 }
