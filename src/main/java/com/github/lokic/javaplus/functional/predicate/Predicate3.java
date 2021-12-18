b93e6f96f034f90e8ad29b4bddede4ab41bf5a95
loki <kiraconan2046@gmail.com>
2021-12-19 00:24:09 +0800

    feat: Predicate支持特定索引参数的函数，避免编写不关注的变量名

--- src/main/java/com/github/lokic/javaplus/functional/predicate/Predicate3.java
+++ src/main/java/com/github/lokic/javaplus/functional/predicate/Predicate3.java
@@ -1,7 +1,21 @@
 package com.github.lokic.javaplus.functional.predicate;
 
 
+import java.util.function.Predicate;
+
 @FunctionalInterface
 public interface Predicate3<T1, T2, T3> {
     boolean test(T1 t1, T2 t2, T3 t3);
+
+    static <T1, T2, T3> Predicate3<T1, T2, T3> $1(Predicate<T1> predicate) {
+        return (t1, t2, t3) -> predicate.test(t1);
+    }
+
+    static <T1, T2, T3> Predicate3<T1, T2, T3> $2(Predicate<T2> predicate) {
+        return (t1, t2, t3) -> predicate.test(t2);
+    }
+
+    static <T1, T2, T3> Predicate3<T1, T2, T3> $3(Predicate<T3> predicate) {
+        return (t1, t2, t3) -> predicate.test(t3);
+    }
 }
