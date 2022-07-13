a94c991440f74a2909df28e2f1722621a326d7ea
loki <kiraconan2046@gmail.com>
2022-07-13 22:34:52 +0800

    feat: 支持left join的排序，不再兼容老的join方法

--- src/main/java/com/github/lokic/javaplus/join/Join.java
+++ src/main/java/com/github/lokic/javaplus/join/Join.java
@@ -8,21 +8,4 @@ public class Join {
         return new JoinStream<>(stream);
     }
 
-    public static <T1, T2> JoinType<T1, T2> innerJoin(Stream<T1> left, Stream<T2> right) {
-        return new JoinType<>(left, right, t -> t.getT1() != null && t.getT2() != null);
-    }
-
-    public static <T1, T2> JoinType<T1, T2> leftOuterJoin(Stream<T1> left, Stream<T2> right) {
-        return new JoinType<>(left, right, t -> t.getT1() != null);
-    }
-
-    public static <T1, T2> JoinType<T1, T2> rightOuterJoin(Stream<T1> left, Stream<T2> right) {
-        return new JoinType<>(left, right, t -> t.getT2() != null);
-    }
-
-    public static <T1, T2> JoinType<T1, T2> fullOuterJoin(Stream<T1> left, Stream<T2> right) {
-        return new JoinType<>(left, right, t -> !(t.getT1() == null && t.getT2() == null));
-    }
-
-
 }
--- src/main/java/com/github/lokic/javaplus/join/JoinType.java
+++ src/main/java/com/github/lokic/javaplus/join/JoinType.java
@@ -1,5 +1,6 @@
 package com.github.lokic.javaplus.join;
 
+import com.github.lokic.javaplus.Functions;
 import com.github.lokic.javaplus.NullData;
 import com.github.lokic.javaplus.tuple.Tuple;
 import com.github.lokic.javaplus.tuple.Tuple2;
@@ -13,23 +14,23 @@ import java.util.stream.Stream;
 
 public class JoinType<T1, T2> {
 
-    private final Stream<Tuple2<T1, T2>> leftWrappedStream;
-    private final Stream<Tuple2<T1, T2>> rightWrappedStream;
+    private final Stream<Tuple2<Integer, Tuple2<T1, T2>>> leftWrappedStream;
+    private final Stream<Tuple2<Integer, Tuple2<T1, T2>>> rightWrappedStream;
     private final Predicate<Tuple2<T1, T2>> joinMatcher;
 
     JoinType(Stream<T1> left, Stream<T2> right, Predicate<Tuple2<T1, T2>> joinMatcher) {
-        this.leftWrappedStream = left.map(l -> Tuple.of(Objects.requireNonNull(l), null));
-        this.rightWrappedStream = right.map(r -> Tuple.of(null, Objects.requireNonNull(r)));
+        this.leftWrappedStream = left.map(l -> Tuple.of(Objects.requireNonNull(l), (T2) null)).map(Functions.mapWithIndex(Tuple::of));
+        this.rightWrappedStream = right.map(r -> Tuple.of((T1) null, Objects.requireNonNull(r))).map(Functions.mapWithIndex(Tuple::of));
         this.joinMatcher = joinMatcher;
     }
 
     public <K> JoinStream<Tuple2<T1, T2>> on(Function<T1, K> leftKey, Function<T2, K> rightKey) {
         Stream<Tuple2<T1, T2>> stream = Stream.concat(leftWrappedStream, rightWrappedStream)
                 .collect(Collectors.toMap(
-                        t -> matchKey(t, leftKey, rightKey),
+                        t -> matchKey(t.getT2(), leftKey, rightKey),
                         Collections::singletonList,
                         (a, b) -> {
-                            List<Tuple2<T1, T2>> li = new ArrayList<>();
+                            List<Tuple2<Integer, Tuple2<T1, T2>>> li = new ArrayList<>();
                             li.addAll(a);
                             li.addAll(b);
                             return li;
@@ -39,7 +40,9 @@ public class JoinType<T1, T2> {
                 .values()
                 .stream()
                 .flatMap(this::cartesian)
-                .filter(this.joinMatcher);
+                .sorted(this::compare)
+                .filter(t -> joinMatcher.test(t.getT2()))
+                .map(Tuple2::getT2);
         return new JoinStream<>(stream);
     }
 
@@ -49,18 +52,21 @@ public class JoinType<T1, T2> {
      * @param li
      * @return
      */
-    private Stream<Tuple2<T1, T2>> cartesian(List<Tuple2<T1, T2>> li) {
-        Map<Boolean, List<Tuple2<T1, T2>>> map = li.stream()
-                .collect(Collectors.partitioningBy(this::isLeft, this.toListOrNullList()));
-        List<Tuple2<T1, T2>> left = map.get(true);
-        List<Tuple2<T1, T2>> right = map.get(false);
+    private Stream<Tuple2<Tuple2<Integer, Integer>, Tuple2<T1, T2>>> cartesian(List<Tuple2<Integer, Tuple2<T1, T2>>> li) {
+        Map<Boolean, List<Tuple2<Integer, Tuple2<T1, T2>>>> map = li.stream()
+                .collect(Collectors.partitioningBy(t -> isLeft(t.getT2()), this.toListOrNullList()));
+        List<Tuple2<Integer, Tuple2<T1, T2>>> left = map.get(true);
+        List<Tuple2<Integer, Tuple2<T1, T2>>> right = map.get(false);
         return left.stream()
                 .flatMap(l -> right.stream()
                         .map(r -> this.merge(l, r)));
     }
 
-    private Tuple2<T1, T2> merge(Tuple2<T1, T2> l, Tuple2<T1, T2> r) {
-        return Tuple.of(l == null ? null : l.getT1(), r == null ? null : r.getT2());
+
+    private Tuple2<Tuple2<Integer, Integer>, Tuple2<T1, T2>> merge(Tuple2<Integer, Tuple2<T1, T2>> l, Tuple2<Integer, Tuple2<T1, T2>> r) {
+        return Tuple.of(
+                Tuple.of(l == null ? null : l.getT1(), r == null ? null : r.getT1()),
+                Tuple.of(l == null ? null : l.getT2().getT1(), r == null ? null : r.getT2().getT2()));
     }
 
     private boolean isLeft(Tuple2<T1, T2> t) {
@@ -93,4 +99,30 @@ public class JoinType<T1, T2> {
                 }
         );
     }
+
+    private int compare(Tuple2<Tuple2<Integer, Integer>, Tuple2<T1, T2>> a, Tuple2<Tuple2<Integer, Integer>, Tuple2<T1, T2>> b) {
+        Tuple2<Integer, Integer> ta = a.getT1();
+        Tuple2<Integer, Integer> tb = b.getT1();
+
+        int cmp = compare(ta.getT1(), tb.getT1());
+        if (cmp == 0) {
+            return compare(ta.getT2(), tb.getT2());
+        }
+        return cmp;
+    }
+
+
+    private int compare(Integer a, Integer b) {
+        if (a == null && b == null) {
+            return 0;
+        }
+        if (a == null) {
+            return 1;
+        }
+        if (b == null) {
+            return -1;
+        }
+        return Integer.compare(a, b);
+    }
+
 }
