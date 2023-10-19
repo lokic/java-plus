b8e74fb7f464cdc2902c6eeb963f097ab3030c93
loki <kiraconan2046@gmail.com>
2023-10-19 12:19:37 +0800

    feat: upgrade field, key to keyCode

--- src/main/java/com/github/lokic/javaplus/property/KeyEnum.java
+++ src/main/java/com/github/lokic/javaplus/property/KeyEnum.java
@@ -9,9 +9,9 @@ public interface KeyEnum<E extends Enum<E> & KeyEnum<E>> {
 
     TypeSafeMap CACHE = new TypeSafeMap(new ConcurrentHashMap<>());
 
-    int getKey();
+    int getKeyCode();
 
-    static <T extends Enum<T> & KeyEnum<T>> T keyOf(Class<T> enumType, Integer key) {
+    static <T extends Enum<T> & KeyEnum<T>> T keyCodeOf(Class<T> enumType, Integer key) {
         return getProperty(enumType).of(key);
     }
 
@@ -19,7 +19,7 @@ public interface KeyEnum<E extends Enum<E> & KeyEnum<E>> {
         return CACHE.<Class<T>, Property1<T, Integer>>getMap()
                 .computeIfAbsent(enumType, SneakyThrowsFunctional.function(t -> {
                     //Note: JDK-8141508 bug，必须写成 ke -> ke.getKey()，不要使用方法引用
-                    return new Property1<>(enumType, ke -> ke.getKey());
+                    return new Property1<>(enumType, ke -> ke.getKeyCode());
                 }));
     }
 
