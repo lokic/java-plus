732a93851a4cb8423e8d2a96ec062e1c2ed2a132
loki <kiraconan2046@gmail.com>
2023-10-24 01:25:51 +0800

    feat: add method to FunctionalInterface

--- src/main/java/com/github/lokic/javaplus/Functions.java
+++ src/main/java/com/github/lokic/javaplus/Functions.java
@@ -1,6 +1,9 @@
 package com.github.lokic.javaplus;
 
+import java.lang.invoke.*;
+import java.lang.reflect.Method;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
@@ -21,4 +24,29 @@ public class Functions {
         AtomicInteger i = new AtomicInteger();
         return t -> biFunction.apply(i.getAndIncrement(), t);
     }
+
+    @SuppressWarnings("unchecked")
+    public static <T, V> BiConsumer<T, V> toBiConsumer(Method method, Class<T> clazz, Class<V> vClass) throws Throwable {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodHandle handle = lookup.unreflect(method);
+        final CallSite site = LambdaMetafactory.metafactory(lookup, "accept",
+                MethodType.methodType(BiConsumer.class),
+                MethodType.methodType(void.class, Object.class, Object.class),
+                handle,
+                MethodType.methodType(void.class, clazz, vClass));
+        return (BiConsumer<T, V>) site.getTarget().invokeExact();
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T, R> Function<T, R> toFunction(Method method, Class<T> clazz, Class<R> retClass) throws Throwable {
+        MethodHandles.Lookup lookup = MethodHandles.lookup();
+        MethodHandle handle = lookup.unreflect(method);
+        final CallSite site = LambdaMetafactory.metafactory(lookup, "apply",
+                MethodType.methodType(Function.class),
+                MethodType.methodType(Object.class, Object.class),
+                handle,
+                MethodType.methodType(retClass, clazz));
+        return (Function<T, R>) site.getTarget().invokeExact();
+    }
+
 }
--- src/test/java/com/github/lokic/javaplus/FunctionsTest.java
+++ src/test/java/com/github/lokic/javaplus/FunctionsTest.java
@@ -1,8 +1,12 @@
 package com.github.lokic.javaplus;
 
+import lombok.SneakyThrows;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
+import java.lang.reflect.Method;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,4 +22,54 @@ public class FunctionsTest {
                 .containsExactly("A : 0", "B : 1", "C : 2");
     }
 
+    @SneakyThrows
+    @Test
+    public void testToBiConsumer() {
+        Method m = TestClass.class.getDeclaredMethod("setId", Long.class);
+        BiConsumer<TestClass, Long> f = Functions.toBiConsumer(m, TestClass.class, Long.class);
+        TestClass a = new TestClass(123L);
+        f.accept(a, 1L);
+
+        Assertions.assertThat(a.getId())
+                .isEqualTo(1L);
+
+        f.accept(a, null);
+        Assertions.assertThat(a.getId())
+                .isEqualTo(null);
+
+    }
+
+    @SneakyThrows
+    @Test
+    public void testToFunction() {
+        Method m = TestClass.class.getDeclaredMethod("getId");
+        Function<TestClass, Long> f = Functions.toFunction(m, TestClass.class, Long.class);
+
+        Assertions.assertThat(f.apply(new TestClass(123L)))
+                .isEqualTo(123L);
+        Assertions.assertThat(f.apply(new TestClass(1L)))
+                .isEqualTo(1L);
+        Assertions.assertThat(f.apply(new TestClass()))
+                .isEqualTo(null);
+    }
+
+    public static class TestClass {
+        private Long id;
+
+        public TestClass() {
+
+        }
+
+        public TestClass(Long id) {
+            this.id = id;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
 }
