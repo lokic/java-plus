c14bc2592aee42bad92b5825fd5a82a1df6126eb
loki <kiraconan2046@gmail.com>
2023-10-23 23:30:51 +0800

    feat: add BigDecimals

--- src/main/java/com/github/lokic/javaplus/BigDecimals.java
+++ src/main/java/com/github/lokic/javaplus/BigDecimals.java
@@ -10,6 +10,13 @@ public class BigDecimals {
         return a.compareTo(BigDecimal.valueOf(b)) == 0;
     }
 
+    public static boolean eq(BigDecimal a, Long b) {
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.compareTo(BigDecimal.valueOf(b)) == 0;
+    }
+
     public static boolean eq(BigDecimal a, BigDecimal b) {
         if (a == null || b == null) {
             return false;
@@ -19,6 +26,13 @@ public class BigDecimals {
 
 
     public static boolean le(BigDecimal a, Integer b) {
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.compareTo(BigDecimal.valueOf(b)) <= 0;
+    }
+
+    public static boolean le(BigDecimal a, Long b) {
         if (a == null || b == null) {
             return false;
         }
@@ -39,6 +53,13 @@ public class BigDecimals {
         return a.compareTo(BigDecimal.valueOf(b)) < 0;
     }
 
+    public static boolean lt(BigDecimal a, Long b) {
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.compareTo(BigDecimal.valueOf(b)) < 0;
+    }
+
     public static boolean lt(BigDecimal a, BigDecimal b) {
         if (a == null || b == null) {
             return false;
@@ -53,6 +74,13 @@ public class BigDecimals {
         return a.compareTo(BigDecimal.valueOf(b)) >= 0;
     }
 
+    public static boolean ge(BigDecimal a, Long b) {
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.compareTo(BigDecimal.valueOf(b)) >= 0;
+    }
+
     public static boolean ge(BigDecimal a, BigDecimal b) {
         if (a == null || b == null) {
             return false;
@@ -61,6 +89,13 @@ public class BigDecimals {
     }
 
     public static boolean gt(BigDecimal a, Integer b) {
+        if (a == null || b == null) {
+            return false;
+        }
+        return a.compareTo(BigDecimal.valueOf(b)) > 0;
+    }
+
+    public static boolean gt(BigDecimal a, Long b) {
         if (a == null || b == null) {
             return false;
         }
